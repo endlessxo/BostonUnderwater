f5db9d402720267ba1f0d5812399f47450b5fb23
Kenneth Mui <kingfmui@bu.edu>
2014-10-24 20:29:48 -0400

    It works! Just put the js/css files in /django_project/django_project/static

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'signups',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -84,3 +85,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
